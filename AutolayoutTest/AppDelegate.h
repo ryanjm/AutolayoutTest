ee58d1fd9966bf6dfe44b4d708e3de447618dc66
Ryan Mathews <ryan@ryanjm.com>
2015-05-11 10:33:45 -0600

    Initial Commit

---
+++ AutolayoutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutTest
+//
+//  Created by Ryan Mathews on 5/11/15.
+//  Copyright (c) 2015 Ryan Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
